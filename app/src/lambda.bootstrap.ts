724ade337a1e18aeb17e3dd6ab46fde348ec04e6
Taranjeet Singh <150602288+taranjeet-s@users.noreply.github.com>
2024-03-02 16:54:21 +1100

    updated serverless-express
    removed @vendia/serverless-express and installed @codegenie/serverless-express

--- app/src/lambda.bootstrap.ts
+++ app/src/lambda.bootstrap.ts
@@ -1,42 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import * as express from 'express';
-import * as serverlessExpress from '@vendia/serverless-express';
+import * as serverlessExpress from '@codegenie/serverless-express';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 let cachedServer: any;
 
-// function requestMapper(params: any) {
-//   const { event } = params;
-//   console.log('Request event', event);
-
-//   return {
-//     method: event.httpMethod,
-//     path: event.path,
-//     headers: event.headers,
-//     requestContext: event.requestContext,
-//     body: event.body,
-//   };
-// }
-
-// function responseMapper(params: any) {
-//   console.log(' Response Params', params);
-//   const { event } = params;
-//   // Your logic here...
-
-//   return {
-//     statusCode: event.statusCode,
-//     body: event.body,
-//     headers: event.headers,
-//   };
-// }
-
 export const bootstrapLambda = async (
   attachPipes: (app: INestApplication<any>) => void,
 ) => {
   if (!cachedServer) {
-    console.log('Creating serverless server v6');
     // create an express app
     const expressApp = express();
 
@@ -56,15 +30,8 @@ export const bootstrapLambda = async (
     // create a serverless server using serverless express
     cachedServer = serverlessExpress.configure({ app: expressApp });
 
-    // cachedServer = serverlessExpress.configure({
-    //   app: expressApp,
-    //   eventSource: {
-    //     getRequest: requestMapper,
-    //     getResponse: responseMapper,
-    //   },
-    // });
     return cachedServer;
   }
-  console.log('Using cached serverless server v6');
+
   return cachedServer;
 };
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -2,7 +2,6 @@ import * as cdk from "aws-cdk-lib";
 import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
 import { DockerImageCode, DockerImageFunction } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
-// import * as iam from "aws-cdk-lib/aws-iam";
 import * as logs from "aws-cdk-lib/aws-logs";
 import path = require("path");
 
@@ -11,60 +10,53 @@ export class CdkStack extends cdk.Stack {
     super(scope, id, props);
 
     // Create a new lambda function
-    const ApiHandler = new DockerImageFunction(this, "ApiHandler1", {
+    const LambdaHandler = new DockerImageFunction(this, "LambdaHandler", {
       timeout: cdk.Duration.seconds(30),
-      functionName: "ApiHandler1",
+      functionName: "LambdaHandler",
       code: DockerImageCode.fromImageAsset(path.join(__dirname, "../../app")),
     });
 
     // Create a new Log Group and Log Stream for the Lambda function
-    new logs.LogGroup(this, "ApiHandlerLogGroup1", {
-      logGroupName: `/aws/lambda/${ApiHandler.functionName}`,
+    new logs.LogGroup(this, "LambdaHandlerLogGroup", {
+      logGroupName: `/aws/lambda/${LambdaHandler.functionName}`,
       retention: logs.RetentionDays.ONE_WEEK,
     });
 
     // Create a new api gateway
-    const api = new RestApi(this, "ApiRest1", {
-      restApiName: "ApiRest1",
+    const api = new RestApi(this, "ApiRest", {
+      restApiName: "ApiRest",
       deploy: true,
       defaultMethodOptions: {
-        apiKeyRequired: false,
+        apiKeyRequired: true,
       },
     });
 
     // add proxy resource to handle all api requests
     api.root.addProxy({
-      defaultIntegration: new LambdaIntegration(ApiHandler, {
+      defaultIntegration: new LambdaIntegration(LambdaHandler, {
         proxy: true,
       }),
     });
 
-    // const restApi = new RestApi(this, "ApiRest1", {
-    //   restApiName: "ApiRest1",
-    // });
-
-    // const proxyResource = restApi.root.addResource("{proxy+}"); // Catch-all for any subpath
-    // proxyResource.addMethod("ANY", new LambdaIntegration(ApiHandler));
-
     //add api key to enable monitoring
-    // const apiKey = api.addApiKey("ApiKey");
-    // const usagePlan = api.addUsagePlan("ApiUsagePlan", {
-    //   name: "ApiUsagePlan",
-    //   apiStages: [
-    //     {
-    //       api,
-    //       stage: api.deploymentStage,
-    //     },
-    //   ],
-    // });
+    const apiKey = api.addApiKey("ApiKey");
+    const usagePlan = api.addUsagePlan("ApiUsagePlan", {
+      name: "ApiUsagePlan",
+      apiStages: [
+        {
+          api,
+          stage: api.deploymentStage,
+        },
+      ],
+    });
 
     // // add the api key to the usage plan
-    // usagePlan.addApiKey(apiKey);
+    usagePlan.addApiKey(apiKey);
 
-    // // add the api key to the output
-    // new cdk.CfnOutput(this, "failed-requests-arn", {
-    //   value: apiKey.keyId,
-    //   exportName: `keyId`,
-    // });
+    // add the api key to the output
+    new cdk.CfnOutput(this, "api-key", {
+      value: apiKey.keyId,
+      exportName: `keyId`,
+    });
   }
 }
